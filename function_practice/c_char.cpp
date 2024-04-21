ab4a893577e34942b3128ce2dbd2077bfbbc7996
AlexHchs <alexhchs198720@gmail.com>
2024-04-21 16:10:24 +0800

    240421

--- function_practice/c_char.cpp
+++ function_practice/c_char.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main(){
     char str1[2] = {'a', 'b'}, str2[2] = {'a'};
+    char* ptr = str1+2;
+    char jud;
 
+    jud = (*ptr == '\0') ? 't' : 'f';
 
-    printf("Output: str1 = %c, %c", str1[0], str1[1]);
+    printf("Output: str1 = %c", jud);
 
-    char* ptr = str1;
     
 
     return 0;
